5793f8ffe46082ec59c1da29f2f94da48fd33101
Hitesh Rajpal <hiteshrajpal1904@gmail.com>
2020-07-25 13:15:23 +0530

    Updates

---
+++ precache-manifest.be9284c18641f02c5042acf70f2459e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ecca53b34d52118bd2d21247bfe8e4e",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "42ed1cd2a504f187d2fd",
+    "url": "/profile/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e9d82b108e626ab1ced6",
+    "url": "/profile/static/js/2.bd5fb903.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/profile/static/js/2.bd5fb903.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42ed1cd2a504f187d2fd",
+    "url": "/profile/static/js/main.dcaa072d.chunk.js"
+  },
+  {
+    "revision": "5076af70b647b2393d8e",
+    "url": "/profile/static/js/runtime-main.8771bb24.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/profile/static/media/logo.5d5d9eef.svg"
+  }
+]);
